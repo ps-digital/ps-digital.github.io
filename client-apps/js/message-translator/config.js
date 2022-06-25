df805da1219ae601f290a41eb578c4951ca92c9e
Bilal Zaghloul <bilal.zaghloul@genesys.com>
2022-06-26 00:22:48 +0200

    added support center

---
+++ client-apps/js/message-translator/config.js
@@ -0,0 +1,13 @@
+export default {
+    // Token Implicit Grant Client ID
+    clientID: '9049644e-ef61-4c1a-8260-7d3d28548a3f',
+    redirectUri: 'https://dev.localhost.com:5000/client-apps/message-translator.html',
+
+    genesysCloud: {
+        // Genesys Cloud region
+        // eg. 'mypurecloud.ie', 'euw2.pure.cloud', etc...
+        region: 'mypurecloud.de',
+    },
+
+    translateServiceURI: 'https://translategc.demo.genesys.com/translate',
+};
